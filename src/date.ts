dcb0b9d43527f7e2921ad0438b64ca178d1104f3
cody <cevek@ya.ru>
2019-03-15 02:04:58 +0500

    Fix date parser

--- src/date.ts
+++ src/date.ts
@@ -1,13 +1,28 @@
-import { GraphQLScalarType, Kind } from 'graphql';
+import {GraphQLScalarType, Kind, GraphQLError} from 'graphql';
 
 export const DateType = new GraphQLScalarType({
     name: 'Date',
-    serialize: val => val.toJSON(),
-    parseValue: val => new Date(val),
+    serialize: date => {
+        if (Number.isNaN(date.getTime())) {
+            throw new Error('Invalid response date');
+        }
+        return date.toJSON();
+    },
+    parseValue: val => {
+        return parse(val);
+    },
     parseLiteral(ast) {
         if (ast.kind === Kind.STRING) {
-            return new Date(ast.value);
+            return parse(ast.value);
         }
         return null;
     },
 });
+
+function parse(val: string) {
+    const date = new Date(val);
+    if (val.length !== 24 || Number.isNaN(date.getTime())) {
+        throw new GraphQLError('Incorrect Date: ' + val);
+    }
+    return date;
+}
